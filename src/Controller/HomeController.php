586e91ed9cbfba9cb76ed60ca20152734a126a38
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-12-10 21:56:52 +0100

    added a past exhibition section

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -35,14 +35,15 @@ class HomeController extends AbstractController
     #[Route('/exhibitions', name: 'exhibitions')]
     public function exhibitions(): Response
     {
-        $exhibitions = [
+        // Current Exhibitions
+        $currentExhibitions = [
             [
                 'title' => '“La République (Cynique)”',
                 'location' => 'Palais de Tokyo',
                 'city' => 'Paris',
                 'dates' => '13.11 / 01.12.24',
                 'url' => 'https://palaisdetokyo.com/exposition/la-republique-cynique/',
-                'image' => '/img/tokyo.png'
+                'image' => '/img/tokyo.png',
             ],
             [
                 'title' => '“Dirty Rains”',
@@ -50,7 +51,7 @@ class HomeController extends AbstractController
                 'city' => 'Strasbourg',
                 'dates' => '05.10.24 / 23.02.25',
                 'url' => 'https://ceaac.org/fr/exposition/dirty-rains/',
-                'image' => '/img/evenement-strasbourg_502833804.jpg'
+                'image' => '/img/evenement-strasbourg_502833804.jpg',
             ],
             [
                 'title' => 'Photo Saint Germain',
@@ -58,13 +59,58 @@ class HomeController extends AbstractController
                 'city' => 'Paris',
                 'dates' => '11.07 / 11.10.2024',
                 'url' => 'http://www.photosaintgermain.com/editions/2024/parcours/hotel-la-louisiane-roomservice',
-                'image' => '/img/room_service.png'
+                'image' => '/img/room_service.png',
+            ],
+        ];
+    
+        // Past Exhibitions
+        $pastExhibitions = [
+            [
+                'title' => '“Echoes of Time”',
+                'location' => 'MoMA',
+                'city' => 'New York',
+                'dates' => '15.03 / 10.06.23',
+                'url' => 'https://moma.org/exhibition/echoes-of-time',
+                'image' => '/img/moma_echoes.png',
+            ],
+            [
+                'title' => '“Fleeting Shadows”',
+                'location' => 'Tate Modern',
+                'city' => 'London',
+                'dates' => '01.09 / 30.11.23',
+                'url' => 'https://tate.org.uk/exhibition/fleeting-shadows',
+                'image' => '/img/tate_fleeting_shadows.png',
+            ],
+            [
+                'title' => '“Past Horizons”',
+                'location' => 'The Louvre',
+                'city' => 'Paris',
+                'dates' => '10.01 / 20.04.22',
+                'url' => 'https://louvre.fr/exhibition/past-horizons',
+                'image' => '/img/louvre_past_horizons.png',
+            ],
+            [
+                'title' => '“Visions Beyond”',
+                'location' => 'Getty Center',
+                'city' => 'Los Angeles',
+                'dates' => '12.05 / 15.08.22',
+                'url' => 'https://getty.edu/exhibition/visions-beyond',
+                'image' => '/img/getty_visions.png',
+            ],
+            [
+                'title' => '“Whispers of Light”',
+                'location' => 'Rijksmuseum',
+                'city' => 'Amsterdam',
+                'dates' => '20.09 / 05.12.22',
+                'url' => 'https://rijksmuseum.nl/exhibition/whispers-of-light',
+                'image' => '/img/rijksmuseum_whispers.png',
             ],
         ];
     
         return $this->render('home/exhibitions.html.twig', [
             'menuItems' => $this->getMenuItems(),
-            'exhibitions' => $exhibitions,
+            'currentExhibitions' => $currentExhibitions,
+            'pastExhibitions' => $pastExhibitions,
         ]);
     }
 
