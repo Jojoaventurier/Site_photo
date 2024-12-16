a40773842e39c9f6ece862abfa38221846c4489b
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-12-16 21:28:02 +0100

    added message form to the contact page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,16 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Message;
+use App\Form\MessageType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Mime\Email;
+
 
 class HomeController extends AbstractController
 {
@@ -131,10 +138,36 @@ class HomeController extends AbstractController
     }
 
     #[Route('/contact-info', name: 'contact_info')]
-    public function contactInfo(): Response
-    {
+    public function contactInfo(
+        Request $request, 
+        EntityManagerInterface $em, 
+        MailerInterface $mailer
+    ): Response {
+        $contact = new Message();
+        $form = $this->createForm(MessageType::class, $contact);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($contact);
+            $em->flush();
+
+            // Send email
+            $email = (new Email())
+                ->from($contact->getEmail())
+                ->to('marianne.maric@example.com')
+                ->subject('New Contact Form Message')
+                ->text("From: {$contact->getName()}\n\n{$contact->getMessage()}");
+
+            $mailer->send($email);
+
+            $this->addFlash('success', 'Your message has been sent successfully!');
+            return $this->redirectToRoute('contact');
+        }
+
         return $this->render('home/contact_info.html.twig', [
             'menuItems' => $this->getMenuItems(),
+            'form' => $form->createView(),
         ]);
     }
 
