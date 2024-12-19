58904410daf0c5e6174ca61113de6696bc9461ac
Geoffroy Maurutto <165882140+Jojoaventurier@users.noreply.github.com>
2024-12-19 16:39:32 +0100

    created the article entity (same as books but for magazine, newspapers or websites or blog articles)

---
+++ src/Entity/Article.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ArticleRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ArticleRepository::class)]
+class Article
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $author = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $publisherName = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $publicationDate = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $consultLink = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $downloadLink = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?\DateTimeImmutable $createdAt = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getAuthor(): ?string
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?string $author): static
+    {
+        $this->author = $author;
+
+        return $this;
+    }
+
+    public function getPublisherName(): ?string
+    {
+        return $this->publisherName;
+    }
+
+    public function setPublisherName(?string $publisherName): static
+    {
+        $this->publisherName = $publisherName;
+
+        return $this;
+    }
+
+    public function getPublicationDate(): ?\DateTimeInterface
+    {
+        return $this->publicationDate;
+    }
+
+    public function setPublicationDate(?\DateTimeInterface $publicationDate): static
+    {
+        $this->publicationDate = $publicationDate;
+
+        return $this;
+    }
+
+    public function getConsultLink(): ?string
+    {
+        return $this->consultLink;
+    }
+
+    public function setConsultLink(?string $consultLink): static
+    {
+        $this->consultLink = $consultLink;
+
+        return $this;
+    }
+
+    public function getDownloadLink(): ?string
+    {
+        return $this->downloadLink;
+    }
+
+    public function setDownloadLink(?string $downloadLink): static
+    {
+        $this->downloadLink = $downloadLink;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(?\DateTimeImmutable $createdAt): static
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
